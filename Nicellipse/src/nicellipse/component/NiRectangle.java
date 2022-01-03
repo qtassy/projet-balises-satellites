9cbab9d5f3c92c312d8056238e759b07c68b30e6
qtassy <tassyquentin.17@gmail.com>
2022-01-03 11:56:36 +0100

    Initial commit

---
+++ Nicellipse/src/nicellipse/component/NiRectangle.java
@@ -0,0 +1,13 @@
+package nicellipse.component;
+
+import javax.swing.JPanel;
+
+public class NiRectangle extends JPanel implements NiBorderedComponent {
+	private static final long serialVersionUID = 128422045550852289L;
+
+	public NiRectangle() {
+		this.defaultSetup();
+		this.setLayout(null);
+	}
+	
+}
